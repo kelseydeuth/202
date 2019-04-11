9ace5fcb62f32176876ae8097bda527902b7e2b7
Kelsey Deuth <kelseydeuth@Kelseys-MacBook-Pro.local>
2019-04-10 23:31:02 -0700

    automated upload

--- Project1/perm_lex.py
+++ Project1/perm_lex.py
@@ -1,4 +1,15 @@
-def perm_gen_lex(a): 
-    for char in a:
-
+def perm_gen_lex(a):
+    a = list(a)
+    newlist = []
+    if len(a) == 1:
+        return a
+    if len(a) == 0:
+        return a
+    else:
+        for char in range(len(a)):
+            first = a[char]
+            rest = a[:char] + a[char + 1:]
+            for order in perm_gen_lex(rest):
+                newlist.append(first + order)
+        return newlist
  
