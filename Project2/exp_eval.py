11a642ce7c29131e77164bf04a9cfbb1972f1b30
Kelsey Deuth <kelseydeuth@Kelseys-MacBook-Pro.local>
2019-04-24 20:01:03 -0700

    automated upload

--- Project2/exp_eval.py
+++ Project2/exp_eval.py
@@ -25,15 +25,21 @@ def postfix_eval(input_str):
     for num in input_str:
         if num not in ops:
             stack.push(num)  # pushes value onto stack
-        if num in ops and size(stack) < 2:
+        elif num in ops and stack.size() < 2:
             raise PostfixFormatException
         else:  # if the element is a operator, pop operands for the operator from stack.
             num1 = int(stack.pop())
             num2 = int(stack.pop())
             new = do_math(num, num1, num2)  # evaluate the operator and push the result back to the stack
             stack.push(new)
+    error1 = "Insufficient operands"
+    error2 = "Too many operands"
     if stack.size() > 1:
-        raise PostfixFormatException
+        if stack.peek() in ops:
+            return error1
+        else:
+            return error2
+
     return stack   # when the expression is ended, the number in the stack is the final answer
 
 
@@ -74,13 +80,14 @@ def infix_to_postfix(input_str):
                 output_expression.append(op_stack.pop())
             op_stack.pop()
         if num in ops:
-            while order[num] >= op_stack.peek():
+            while op_stack.size() > 0 and order[num] >= op_stack.peek():
                 output_expression.append(op_stack.pop())  # remove operators on the stack; append them to the output
             op_stack.push(num)
     while op_stack.is_empty() is not True:  # when the input expression has been processed, check the op_stack.
         n = op_stack.pop()
         output_expression.append(n)  # any operators still on the stack are removed and appended to output stack.
-    return output_expression
+    new = ' '.join(output_expression)
+    return new
 
 
 # infix_to_postfix('3 + 4 * 2 / (1 - 5) ** 2 **3')
@@ -94,18 +101,20 @@ def prefix_to_postfix(input_str):
     input_str = input_str.split(" ")
     op_stack = Stack(30)
     ops = ['+', '-', '*', '/', '**']
-    for num in input_str[-1]:
+    for num in input_str[::-1]:
         if num not in ops:  # if num is a number, add it to the stack
             op_stack.push(num)
-        if num in ops:
+        else:
             op1 = op_stack.pop()
             op2 = op_stack.pop()
             n = op1 + op2 + num
             stack.push(n)
-        return op_stack
+        return op_stack.pop()
 
 
 # prefix_to_postfix('* - 3 / 2 1 - / 4 5 6')
+
+
 
 
 
