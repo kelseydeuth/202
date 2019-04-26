86ada819433a379d8ab67037e3ee03543fd5330e
Kelsey Deuth <kelseydeuth@Kelseys-MacBook-Pro.local>
2019-04-25 22:09:07 -0700

    automated upload

--- Lab4/ordered_list.py
+++ Lab4/ordered_list.py
@@ -7,8 +7,6 @@
 #Section 11
 #Creates a doubly linked list
 
-
-
 class Node:
     '''Node for use with doubly-linked list'''
     def __init__(self, item):
@@ -30,7 +28,7 @@ class OrderedList:
     def is_empty(self):
         '''Returns back True if OrderedList is empty
             MUST have O(1) performance'''
-        if self.dummy.next is None:
+        if self.dummy.next == self.dummy:
             return True
         return False
 
@@ -43,7 +41,7 @@ class OrderedList:
         cur = self.dummy
         while cur.next != self.dummy.next:
             cur = cur.next
-            if item < cur.data:
+            if item < cur.item:
                 break
         cur.prev.next = n
         n.next = cur
@@ -73,40 +71,46 @@ class OrderedList:
         cur = self.dummy
         while cur.next != self.dummy:
             cur = cur.next
-            idx += 1
             if item == cur.item:
-                return idx - 1
+                return idx
+            idx += 1
+        else:
             return None
 
     def pop(self, index):
         '''Removes and returns item at index (assuming head of list is index 0).
            If index is negative or >= size of list, raises IndexError
            MUST have O(n) average-case performance'''
-        if index < 0 or index > self.size():
+        if index < 0 or index >= self.size():
             raise IndexError
-        else:
-            idx = 0
-            cur = self.dummy
-            while idx != index:
-                cur = cur.next
-                idx += 1
-            return cur.prev.item
-        cur = cur.prev
+        cur = self.dummy.next
+        popped = self.dummy.next.item
+        count = 0
+        while count != index:
+            popped = cur.next.item
+            cur = cur.next
+            count += 1
         cur.prev.next = cur.next
         cur.next.prev = cur.prev
-
+        return popped
 
     def search(self, item):
         '''Searches OrderedList for item, returns True if item is in list, False otherwise"
            To practice recursion, this method must call a RECURSIVE method that
            will search the list
            MUST have O(n) average-case performance'''
-        cur = self.dummy
-        if item == cur.item:
-            return True
-        if cur.next == self.dummy:
+        return self.helper_search(item, self.dummy.next)
+
+    def helper_search(self, item, node):
+        if node == self.dummy:
             return False
-        return self.search(cur.next)
+        elif node.item > item:
+            return False
+        elif node.item == item:
+            return True
+        else:
+            node = node.next
+            return self.helper_search(item, node)
 
     def python_list(self):
         '''Return a Python list representation of OrderedList, from head to tail
@@ -117,7 +121,7 @@ class OrderedList:
         while cur.next != self.dummy:
             cur = cur.next
             lst.append(cur.item)
-        return lst[::-1]
+        return lst
 
     def python_list_reversed(self):
         '''Return a Python list representation of OrderedList, from tail to head, using recursion
@@ -125,12 +129,15 @@ class OrderedList:
            To practice recursion, this method must call a RECURSIVE method that
            will return a reversed list
            MUST have O(n) performance'''
-        lst = []
-        cur = self.dummy
-        while cur.next != self.dummy:
-            cur = cur.next
-            lst.append(cur.item)
-        return lst[::-1]
+        return self.helper_python_list_reversed(self.dummy.prev)
+
+    def helper_python_list_reversed(self, node):
+        lst = [node.item]
+        node = node.prev
+        if node == self.dummy:
+            return lst
+        return lst + self.helper_python_list_reversed(node)
+
 
     def size(self):
         '''Returns number of items in the OrderedList
--- Lab4/ordered_list_tests.py
+++ Lab4/ordered_list_tests.py
@@ -10,7 +10,6 @@
 import unittest
 from ordered_list import *
 
-
 class TestLab4(unittest.TestCase):
 
     def test_simple(self):
@@ -23,10 +22,29 @@ class TestLab4(unittest.TestCase):
         self.assertFalse(t_list.is_empty())
         self.assertEqual(t_list.python_list_reversed(), [10])
         self.assertTrue(t_list.remove(10))
+        self.assertTrue(t_list.is_empty())
+        self.assertFalse(t_list.search(10))
         t_list.add(10)
+        self.assertFalse(t_list.remove(11))
         self.assertEqual(t_list.pop(0), 10)
+        t_list.add(10)
+        t_list.add(12)
+        t_list.add(13)
+        self.assertEqual(t_list.python_list(), [10,12,13])
+        self.assertEqual(t_list.python_list_reversed(), [13,12,10])
+        self.assertEqual(t_list.index(12), 1)
+        self.assertEqual(t_list.index(13), 2)
+        self.assertTrue(t_list.search(13))
+        self.assertFalse(t_list.search(11))
+        self.assertEqual(t_list.index(14), None)
+        with self.assertRaises(IndexError):
+            self.assertEqual(t_list.pop(4))
+        with self.assertRaises(IndexError):
+            self.assertEqual(t_list.pop(-3))
+        self.assertEqual(t_list.pop(2), 13)
 
 
-if __name__ == '__main__': 
-    unittest.main()
+if __name__ == '__main__':
+   unittest.main()
+
 
