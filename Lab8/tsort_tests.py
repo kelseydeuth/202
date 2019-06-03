d4cd1c13d9e21cd6f9d2bd18872d86799024fbf8
Kelsey Deuth <kelsey.deuth@gmail.com>
2019-06-03 13:12:58 -0700

    automated upload

--- Lab8/tsort_tests.py
+++ Lab8/tsort_tests.py
@@ -27,6 +27,46 @@ class TestTsort(unittest.TestCase):
         actual = tsort(input)
         self.assertEqual(actual.strip(), expect)
 
+    def test_simple(self):
+        stack = Stack(5)
+        stack.push(0)
+        self.assertFalse(stack.is_empty())
+        self.assertEqual(stack.size(), 1)
+
+    def test1(self):
+        stack1 = Stack(1)
+        stack1.push(2)
+        self.assertFalse(stack1.is_empty())
+        with self.assertRaises(IndexError):  #checks for exception
+            stack1.push(1)
+
+    def test2(self):
+        stack2 = Stack(5)
+        stack2.push(3)
+        stacktest = Stack(0)
+        self.assertEqual(stack2.pop(), 3)
+        with self.assertRaises(IndexError):  #checks for exception
+            stacktest.pop()
+        with self.assertRaises(IndexError):  #checks for exception
+            stacktest.peek()
+
+    def test3(self):
+        stack3 = Stack(3)
+        stack3.push(7)
+        stack3.push(6)
+        stack3.push(5)
+        stacktest = Stack(0)
+        self.assertEqual(stack3.peek(), 5)
+        self.assertEqual(stack3.size(), 3)
+        self.assertFalse(stack3.is_empty())
+        self.assertTrue(stacktest.is_empty())
+        #stackNone = Stack(None)
+        #with self.assertRaises(IndexError):  #checks for None exception
+        #    stackNone.pop()
+
+
+
+
 if __name__ == "__main__":
     unittest.main()
 
